e855c7af79ddcebff2e206371f5939b06d5df83b
RunningMSN <running.msn.wi@gmail.com>
2022-01-23 12:00:51 -0600

    Fixed ValueError bug in dot plot

--- plotters/dot_graph.py
+++ plotters/dot_graph.py
@@ -1,18 +1,18 @@
 import streamlit as st
 from plotnine import ggplot, aes, geom_point, scale_x_date, theme, labs
 import pandas as pd
-from mizani.breaks import date_breaks
-from mizani.formatters import date_format
 
 def generate(data):
     '''Generates a Cleveland dot plot with all schools and actions for schools.'''
     title_input = st.text_input("Choose a title", value="Application Cycle")
     melted = data.melt(id_vars=data.columns[0], value_vars=data.columns[1:], var_name='Actions', value_name='date')
     height = len(melted.columns[0]) * 1
+    melted['date'] = pd.to_datetime(melted['date'])
     p = (ggplot(melted, aes(x = 'date', y = melted.columns[0], color='Actions'))
          + geom_point()
-         + scale_x_date(breaks = date_breaks('1 months'), labels = date_format('%b'))
          + theme(figure_size=(5, height))
-         + labs(x="", y="", title=title_input))
+         + labs(x="", y="", title=title_input)
+         + scale_x_date(date_breaks = '1 months', date_labels = '%b')
+         )
 
     st.pyplot(ggplot.draw(p))
